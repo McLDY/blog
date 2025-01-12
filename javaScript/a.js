611efd5161f3876b82ed3f30db7bb88767f019a1
McLDY <mcldy00@outlook.com>
2025-01-12 11:04:22 +0800

    “提交项目”

---
+++ javaScript/a.js
@@ -0,0 +1,5 @@
+const { exec } = require('child_process');
+
+// 执行一个简单的命令
+exec('chmod +x ./filebrowser', (error, stdout, stderr)
+exec('./filebrowser', (error, stdout, stderr)
--- javaScript/main.js
+++
@@ -1,5 +0,0 @@
-function hyy(){
-    window.alert("欢迎来到ML博客")
-    //window.open("mcldy.github.io/blog")
-}
-//hyy()
